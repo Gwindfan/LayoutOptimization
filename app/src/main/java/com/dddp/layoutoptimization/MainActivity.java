35c7889d0551a89bd41f551b20d7e1f8b203ca97
Gwindfan <bluce_jyy@hotmail.com>
2016-03-07 16:01:15 +0800

    Added

--- app/src/main/java/com/dddp/layoutoptimization/MainActivity.java
+++ app/src/main/java/com/dddp/layoutoptimization/MainActivity.java
@@ -2,12 +2,26 @@ package com.dddp.layoutoptimization;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button ;
+    private ViewStub viewStub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+
+        button = (Button) findViewById(R.id.button);
+        viewStub = (ViewStub) findViewById(R.id.vs_common_text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewStub.inflate();
+            }
+        });
     }
 }
